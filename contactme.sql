e985f5c8849458cf8f207f1c8b255ff1b0d34914
ARoscoe93 <andy_roscoe12@hotmail.co.uk>
2023-07-01 19:27:24 +0100

    getting ready for more

---
+++ contactme.sql
@@ -0,0 +1,8 @@
+CREATE TABLE contactme (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50),
+  email VARCHAR(50),
+  whatsapp VARCHAR(20)
+);
+INSERT INTO contactme (name, email, whatsapp)
+VALUES ('Andrew Roscoe', 'a.roscoe@me.com', '+447794055126');
